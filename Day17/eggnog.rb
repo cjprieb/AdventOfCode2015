c32eddb8c13b8cc2ad0f8e1f794257d2f10e616c
Crystal Prieb <cjprieb@gmail.com>
2015-12-23 17:32:49 -0600

    Day 17 complete solution

--- Day17/eggnog.rb
+++ Day17/eggnog.rb
@@ -7,32 +7,53 @@ class Eggnog
 		input.map { |line| line.to_i }
 	end
 	
-	def self.valid_combinations(list_of_containers, liters_to_contain)
-		return combinations(list_of_containers, liters_to_contain).combinations
+	def self.valid_combinations(container_list, liters_to_contain)
+		eggnog = Eggnog.new(container_list)
+		return eggnog.count_combinations(container_list, liters_to_contain, 0)
 	end
 	
-	def self.min_containers(list_of_containers, liters_to_contain)
-		result = combinations(list_of_containers, liters_to_contain, {})
-		Result.new()
+	def self.min_containers(container_list, liters_to_contain)
+		eggnog = Eggnog.new(container_list)
+		eggnog.count_combinations(container_list, liters_to_contain, 0)
+		eggnog
+	end	
+	
+	def initialize(container_list)
+		@combo_map = Hash.new(0)
+		@orig_container_list = container_list
+		#puts "original container list length: #{@orig_container_list.length}"
 	end
 	
-	
-	def self.combinations(list_of_containers, liters_to_contain, result)
-		combination_count = 0
-		len = list_of_containers.length
+	def count_combinations(container_list, liters_to_contain, containers_used_count)
+		count = 0
+		len = container_list.length
 		#tabs = "\t" * (5-len)
-		list_of_containers.each_with_index do |container_size, i|
+		container_list.each_with_index do |container_size, i|
+			#puts tabs + "container: #{container_size}"
 			#puts tabs + "#{liters_to_contain} filled a container of size #{container_size}" 
 			leftover = liters_to_contain - container_size
-			leftover_containers = (i+1 < len ? list_of_containers[(i+1)..len] : []) 
+			leftover_containers = (i+1 < len ? container_list[(i+1)..len] : []) 
 			#puts "leftover: " + leftover_containers.to_s
 			if ( leftover == 0 )
+				#puts tabs +"  given list length: #{container_list.length}"
+				#puts tabs +"  leftover list length: #{leftover_containers.length}"
+				#puts tabs +"  containers_used_count: #{containers_used_count}"
+				@combo_map[containers_used_count + 1] += 1
 				count += 1
 			elsif leftover_containers.length > 0
 				#puts tabs + "#{leftover} leftover and #{leftover_containers} to fill" 
-				count += combinations(leftover_containers, leftover)
+				count += count_combinations(leftover_containers, leftover, containers_used_count+1)
 			end
 		end
 		count
+	end
+	
+	def combinations
+		min_key = @combo_map.keys.min
+		@combo_map[min_key]
+	end
+	
+	def containers_count
+		@combo_map.keys.min
 	end
 end
--- Day17/test.rb
+++ Day17/test.rb
@@ -5,11 +5,12 @@ require_relative 'eggnog'
 
 class EggnogTest < Minitest::Test
   def test_parse_1
+   #skip
     assert_equal 4, Eggnog.valid_combinations([20,15,10,5,5], 25)
   end
   def test_parse_2
     result = Eggnog.min_containers([20,15,10,5,5], 25)
-    assert_equal 3, result.combinations
-    assert_equal 2, result.containers_count
+    assert_equal 3, result.combinations, "min combination"
+    assert_equal 2, result.containers_count, "containers_count"
   end
 end
